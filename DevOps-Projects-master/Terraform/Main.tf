b6d1e6fc9c28b428009b6221fc794d42c2162e39
nmahanthi <77907025+nmahanthi@users.noreply.github.com>
2024-06-22 04:42:03 +0530

    testing

--- DevOps-Projects-master/Terraform/Main.tf
+++ DevOps-Projects-master/Terraform/Main.tf
@@ -6,5 +6,8 @@ resource "aws_instance" "example" {
     ami           = "var.ami_id"  
     instance_type = "var.instance_type"
     subnet_id = "var.subnet_id"
-    
+    key_pair_name = "var.key_pair_name"
+      subnet_id = "var.subnet_id"
+      instance_name = "var.instance_name"
+      hosted_zone_name = "var.hosted_zone_name"
 }
