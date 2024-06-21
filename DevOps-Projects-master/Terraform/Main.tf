5275a934956e5d7c4d1d883c0ccffa7da7ab19d4
nmahanthi <77907025+nmahanthi@users.noreply.github.com>
2024-06-22 03:03:19 +0530

    test

--- DevOps-Projects-master/Terraform/Main.tf
+++ DevOps-Projects-master/Terraform/Main.tf
@@ -6,5 +6,5 @@ resource "aws_instance" "example" {
     ami           = "ami-08a0d1e16fc3f61ea"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
     subnet_id = "subnet-05b0a68d7a984b670"
-    key_name = "Docker"
+    
 }
