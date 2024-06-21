21a7d7e875324879c0f732e010c19a65646ad3cb
nmahanthi <77907025+nmahanthi@users.noreply.github.com>
2024-06-22 04:21:23 +0530

    test1

--- DevOps-Projects-master/Terraform/Main.tf
+++ DevOps-Projects-master/Terraform/Main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-east-1"  
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-08a0d1e16fc3f61ea"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
-    subnet_id = "subnet-05b0a68d7a984b670"
+    ami           = "var.ami_id"  
+    instance_type = "var.instance_type"
+    subnet_id = "var.subnet_id"
     
 }
---
+++ DevOps-Projects-master/Terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "instance_type"{
+type = string
+default ="t2.micro"
+}
+variable "ami_id"{
+    type = string
+    
+}
+
+variable "subnet_id"{
+
+    type= string
+}
