166a85994c8885fd9f9230decd3977c4139b6769
nmahanthi <77907025+nmahanthi@users.noreply.github.com>
2024-07-02 00:55:38 +0530

    jenkins

--- DevOps-Projects-master/Terraform/Main.tf
+++ DevOps-Projects-master/Terraform/Main.tf
@@ -4,5 +4,17 @@ provider "aws" {
 
 resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t3.micro"
+    instance_type = "t2.micro"
+}
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "Default subnet for us-west-2a"
+  }
 }
