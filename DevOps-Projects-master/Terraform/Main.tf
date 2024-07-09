6244eb24d3d009999204d39eea40cd0fe54e8884
nmahanthi <77907025+nmahanthi@users.noreply.github.com>
2024-07-10 00:37:54 +0530

    ter

--- DevOps-Projects-master/Terraform/Main.tf
+++ DevOps-Projects-master/Terraform/Main.tf
@@ -6,15 +6,3 @@ resource "aws_instance" "example" {
     ami           = "ami-04b70fa74e45c3917"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
-resource "aws_default_vpc" "default" {
-  tags = {
-    Name = "Default VPC"
-  }
-}
-resource "aws_default_subnet" "default_az1" {
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Default subnet for us-east-1a"
-  }
-}
